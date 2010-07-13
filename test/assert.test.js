be363ae6f7a0577fef7061a17efacf37a91c3a42
Tj Holowaychuk <tj@vision-media.ca>
2010-07-13 11:44:28 -0700

    Added assert.isNull()

--- test/assert.test.js
+++ test/assert.test.js
@@ -7,5 +7,15 @@ module.exports = {
     'assert.includes()': function(assert){
         assert.includes('some random string', 'random');
         assert.includes('some random string', 'dom');
+        assert.throws(function(){
+           assert.include('some random string', 'foobar'); 
+        });
+    },
+    
+    'assert.isNull()': function(assert){
+        assert.isNull(null);
+        assert.throws(function(){
+            assert.isNull(false);
+        });
     }
 };
