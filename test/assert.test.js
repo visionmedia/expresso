1f0908e6536831f1f046c0cb77fca370334cde42
Herbert Vojčík <herby@mailbox.sk>
2010-07-14 02:14:33 -0700

    tests for null/undefined related asserts

--- test/assert.test.js
+++ test/assert.test.js
@@ -1,4 +1,3 @@
-
 module.exports = {
     'assert.eql()': function(assert){
         assert.equal(assert.deepEqual, assert.eql);
@@ -15,6 +14,9 @@ module.exports = {
     'assert.isNull()': function(assert){
         assert.isNull(null);
         assert.throws(function(){
+            assert.isNull(undefined);
+        });
+        assert.throws(function(){
             assert.isNull(false);
         });
     },
@@ -23,6 +25,43 @@ module.exports = {
         assert.isUndefined(undefined);
         assert.throws(function(){
             assert.isUndefined(null);
+        });
+        assert.throws(function(){
+            assert.isUndefined(false);
+        });
+    },
+    
+    'assert.isNotNull()': function(assert){
+        assert.isNotNull(false);
+        assert.isNotNull(undefined);
+        assert.throws(function(){
+            assert.isNotNull(null);
+        });
+    },
+    
+    'assert.isNotUndefined()': function(assert){
+        assert.isNotUndefined(false);
+        assert.isNotUndefined(null);
+        assert.throws(function(){
+            assert.isNotUndefined(undefined);
+        });
+    },
+    
+    'assert.isUndefinedOrNull()': function(assert){
+        assert.isUndefinedOrNull(null);
+        assert.isUndefinedOrNull(undefined);
+        assert.throws(function(){
+            assert.isUndefinedOrNull(false);
+        });
+    },
+    
+    'assert.isNotUndefinedNorNull()': function(assert){
+        assert.isNotUndefinedNorNull(false);
+        assert.throws(function(){
+            assert.isNotUndefinedNorNull(null);
+        });
+        assert.throws(function(){
+            assert.isNotUndefinedNorNull(undefined);
         });
     },
     
