8353e182a6ecba98100016866728d126a0615f36
Tj Holowaychuk <tj@vision-media.ca>
2010-07-15 09:11:30 -0700

    Added timeout support to assert.response(). Closes #3

--- test/http.test.js
+++ test/http.test.js
@@ -7,12 +7,19 @@ var http = require('http');
 
 var server = http.createServer(function(req, res){
     if (req.method === 'GET') {
-        var body = JSON.stringify({ name: 'tj' });
-        res.writeHead(200, {
-            'Content-Type': 'application/json; charset=utf8',
-            'Content-Length': body.length
-        });
-        res.end(body);
+        if (req.url === '/delay') {
+            setTimeout(function(){
+                res.writeHead(200, {});
+                res.end('delayed');
+            }, 200);
+        } else {
+            var body = JSON.stringify({ name: 'tj' });
+            res.writeHead(200, {
+                'Content-Type': 'application/json; charset=utf8',
+                'Content-Length': body.length
+            });
+            res.end(body);
+        }
     } else {
         var body = '';
         req.setEncoding('utf8');
@@ -57,6 +64,10 @@ module.exports = {
             ++called;
             assert.ok(res.body.indexOf('tj') >= 0, 'Test assert.response() callback');
         });
+        
+        assert.response(server,
+            { url: '/delay', timeout: 300 },
+            { body: 'delayed' });
         
         beforeExit(function(){
             assert.equal(2, called);
