bcb011507d3a515d28f79f51578dccaf60a73369
Tj Holowaychuk <tj@vision-media.ca>
2010-08-09 10:42:09 -0700

    Added simple serial reporting

--- test/serial/async.test.js
+++ test/serial/async.test.js
@@ -2,15 +2,19 @@
 module.exports = {
     a: function(assert, done){
         setTimeout(function(){
-            console.log('a');
             done();
         }, 500);
     },
     
     b: function(assert, done){
         setTimeout(function(){
-            console.log('b');
             done();
         }, 200);
+    },
+    
+    c: function(assert, done){
+        setTimeout(function(){
+            done();
+        }, 1000);
     }
 };
