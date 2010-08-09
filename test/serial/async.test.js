5cdf393d0367482948665d0c98d080d85bd4607b
Tj Holowaychuk <tj@vision-media.ca>
2010-08-09 12:46:38 -0700

    Added setup support
    quick / hack ;)

--- test/serial/async.test.js
+++ test/serial/async.test.js
@@ -1,18 +1,28 @@
 
+var setup = 0;
+
 module.exports = {
+    setup: function(done){
+        ++setup;
+        done();
+    },
+
     a: function(assert, done){
+        assert.equal(1, setup);
         setTimeout(function(){
             done();
         }, 500);
     },
     
     b: function(assert, done){
+        assert.equal(2, setup);
         setTimeout(function(){
             done();
         }, 200);
     },
     
     c: function(assert, done){
+        assert.equal(3, setup);
         setTimeout(function(){
             done();
         }, 1000);
