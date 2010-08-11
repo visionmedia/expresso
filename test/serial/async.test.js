46d118b155a7e062149a206f55866b31a031d0e7
Tj Holowaychuk <tj@vision-media.ca>
2010-08-11 09:19:49 -0700

    Added order test

--- test/serial/async.test.js
+++ test/serial/async.test.js
@@ -1,5 +1,6 @@
 
-var setup = 0;
+var setup = 0,
+    order = [];
 
 module.exports = {
     setup: function(done){
@@ -9,6 +10,7 @@ module.exports = {
 
     a: function(assert, done){
         assert.equal(1, setup);
+        order.push('a');
         setTimeout(function(){
             done();
         }, 500);
@@ -16,6 +18,7 @@ module.exports = {
     
     b: function(assert, done){
         assert.equal(2, setup);
+        order.push('b');
         setTimeout(function(){
             done();
         }, 200);
@@ -23,8 +26,13 @@ module.exports = {
     
     c: function(assert, done){
         assert.equal(3, setup);
+        order.push('c');
         setTimeout(function(){
             done();
         }, 1000);
+    },
+
+    d: function(assert){
+        assert.eql(order, ['a', 'b', 'c']);
     }
 };
